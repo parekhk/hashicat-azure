9bfbdc9e6709e03e44ce8339c6da7e6f61b73782
ketan parekh <ketan.parekh@umassmed.edu>
2022-10-14 20:28:04 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -4,5 +4,5 @@ module "network" {
   source  = "parekhk/network/azurerm"
   version = "3.5.0"
   # insert the 1 required variable here
-  resource_group_name = "rg_gaurav"
+  resource_group_name = "${var.prefix}-workshop"
 }
